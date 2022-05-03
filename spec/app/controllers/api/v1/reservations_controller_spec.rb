c002dd4b571df8f7841244a954d2ee72043c1c1a
Prayogo Cendra <prayogocendra@gmail.com>
2022-05-04 01:35:13 +0700

    added reservations controller spec

---
+++ spec/app/controllers/api/v1/reservations_controller_spec.rb
@@ -0,0 +1,26 @@
+require 'rails_helper'
+
+RSpec.describe Api::V1::ReservationsController, type: :controller do
+  describe 'POST upsert' do
+    context 'when given partner alpha payload' do
+      it 'return status 200' do
+        post(:upsert, body: file_fixture('partner_alpha_payload.json').read)
+        expect(response.status).to eq 200
+      end
+    end
+
+    context 'when given partner beta payload' do
+      it 'return status 200' do
+        post(:upsert, body: file_fixture('partner_beta_payload.json').read)
+        expect(response.status).to eq 200
+      end
+    end
+
+    context 'when given malformed payload' do
+      it 'return status 400' do
+        post(:upsert, body: 'not json string')
+        expect(response.status).to eq 400
+      end
+    end
+  end
+end
